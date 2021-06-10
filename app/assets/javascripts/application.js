711d2493be436f0fe850d09c3950336ae2aa3db0
AZUSA5296 <nnfnnfnakuyo731@gmail.com>
2021-06-10 18:00:08 +0000

    [update]投稿数のグラフ化

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,9 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+//= require Chart.min
 //= require rails-ujs
 //= require activestorage
 //= require jquery
-//= require turbolinks
+// require turbolinks
 //= require_tree .
